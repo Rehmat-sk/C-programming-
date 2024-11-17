c290d4f409576bd8c6c89876e6aba4de01682b03
Rehmat-sk <rehmat@eng.rizvi.edu.in>
2024-11-17 20:57:48 +0530

    Add files via upload

---
+++ Experiment 4/exp 4 task 1 me.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int isprimenumber(int num);
+int isprimenumber(int num)
+{
+    for(int i=2;i<num; i++)
+    {
+        if(num%i==0)
+            return 0;}
+
+
+
+    return 1;
+}
+
+
+int main()
+{
+    int start_num, end_num, i;
+    printf("\t\t\t ***PRIME NUMBER FINDER***\n");
+    printf("enter starting number:\n");
+    scanf("%d",&start_num);
+    printf("enter ending number:\n");
+    scanf("%d",&end_num);
+
+    i=start_num<=1?2:start_num;
+     printf("prime numbers between %d and %d\n",start_num,end_num);
+     while(i<= end_num)
+     {
+         if(isprimenumber(i))
+         {     printf("%d\n",i);
+
+         }
+
+       i++;
+    }
+    return 0;
+}
