c2f96f148af457a5e41b91f170faa81bb5539e9b
Rehmat-sk <rehmat@eng.rizvi.edu.in>
2024-11-17 20:59:26 +0530

    Add files via upload

---
+++ Experiment 5/exp5 task2 mee.c
@@ -0,0 +1,26 @@
+//SHAIKH RAHMAT
+//241A046
+//AIDS
+#include<stdio.h>
+int factorial_iterative(int num)
+{
+    int prod = 1;
+    for(int i=1; i<=num; i++)
+    {
+        prod *=i;
+    }
+    return prod;
+}
+
+int main()
+  {
+
+   int num;
+   printf("\t\t\t*** iterative factorial function***\n\n");
+   printf("enter number:");
+   scanf("%d",&num);
+   printf("factorial of %d is %d", num, factorial_iterative(num));
+
+   return 0;
+
+  }
